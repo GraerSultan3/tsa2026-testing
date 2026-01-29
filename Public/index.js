a97c664ba6a10877882f102e2143dbf97c60f046
GraerSultan3 <graer.styne46@stu.bryanisd.org>
2026-01-28 19:14:15 -0600

    Update index.js

--- Public/index.js
+++ Public/index.js
@@ -1,5 +1,11 @@
 const nav = document.getElementsByTagName("nav")[0];
 const title = document.getElementById("title");
+const mainLayout = document.getElementById("mainLayout");
+var height1 = Number.parseFloat(window.getComputedStyle(mainLayout.children[1]).height.substring(0, window.getComputedStyle(mainLayout.children[1]).height.length - 2));
+var height0 = Number.parseFloat(window.getComputedStyle(mainLayout.children[0]).height.substring(0, window.getComputedStyle(mainLayout.children[0]).height.length - 2));
+var endHeight = Math.min(height0, height1);
+var madeElement = document.getElementsByClassName("overflowGrid")[0];
+var biggerElement = (height0 > height1) ? 0 : 1;
 
 nav.style.gridTemplateColumns = `repeat(${nav.children.length - 1}, 1fr)`;
 
@@ -10,4 +16,17 @@ if (nav.children.length % 2 == 1)
 else
 {
     title.style.gridColumn = `${Math.round(nav.children.length / 2)}`;
+}
+
+for (let i = 0; i < mainLayout.children[biggerElement].children.length; i++)
+{
+    let child = mainLayout.children[biggerElement].children[i];
+    if (child.getBoundingClientRect().top + window.scrollY> endHeight)
+    {
+        child.offsetWidth = mainLayout.offsetWidth;
+        madeElement.appendChild(child);
+        console.log(i);
+        i--;
+    }
+    console.log(child.getBoundingClientRect().top + " " + i);
 }
